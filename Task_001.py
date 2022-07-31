2c9274bb2322d7a1873bf92e1a75fafc3edeacbb
Ekaterina <tiurina.kate@gmail.com>
2022-07-31 15:52:30 +0300

    task 001

---
+++ Task_001.py
@@ -0,0 +1,27 @@
+# 1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# *Пример:*
+# - 6782 -> 23
+# - 0,56 -> 11
+
+#  Variant 1
+
+# def sum_of_digit(figure):
+#     sum = 0
+#     for i in str(figure):
+#         if i != ".":
+#             sum += int(i)
+#     return sum
+
+
+# number = float(input('Input a real number: '))
+
+# print(f'Sum of digits {number} is {sum_of_digit(number)}')
+
+
+
+#  Variant 2
+
+number = str(input('Input a real number: '))
+
+number = number.replace('.', '')
+print(sum(map(int,str(number))))
